7bd3dad93b54bb308f23b9c1397ff7d54178f10b
kngenproject <niamniam701@gmail.com>
2026-05-31 14:51:57 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         theme_color: '#0f0f0f',
         background_color: '#0f0f0f',
         display: 'standalone',
-        orientation: 'portrait',
+        orientation: "any",
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
